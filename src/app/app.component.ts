76ae0134b07d72765d613978ada9935516ac097e
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-15 23:06:43 +0330

    use tap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent, map, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables4';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+
+    fromEvent(document, 'click').pipe(
+      map(event => event as MouseEvent),
+      tap( i => console.log(i) ),
+      map( item => {
+        return {
+          x: item.offsetX,
+          y: item.offsetY
+        }
+      }),
+      tap( i => console.log(i) )
+    ).subscribe();
+
+  }
+
 }
